b11bc573952178547181374701fab5d0c6088c04
ariannacostanzo <costanzo.arianna@outlook.it>
2024-04-17 15:37:10 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Questa è una semplicissima applicazione in Python dove puoi inserire le pagine in cui sei registrato e le relative password/nome utente.
+Una volta trovata con successo la password potrà essere incollata dove ti serve con CTRL + v.
+Consiglio di rendere l'applicazione un file .exe
---
+++ main.py
@@ -0,0 +1,26 @@
+import pyperclip
+
+#alcuni esempi, personalizza questo oggetto con i tuoi dati personali
+password = {
+    'facebook': 'pippo',
+    'istagram_nome': 'Maurizio',
+    'istagram_password': 'paperino',
+    'google_email': 'pippoepaperino',
+}
+
+
+while True:
+    print("Quale password ti interessa?")
+    for sito in password:
+        print(f" - {sito}")
+    
+    sito_scelto = input()
+    
+    try:
+        password_scelta = password[sito_scelto]
+        pyperclip.copy(password_scelta)
+        print("\nPassword trovata, sei pronta ad incollarla.")
+        input("Premi Enter per continuare...")
+    except KeyError:
+        #scrivi nello stesso modo in cui leggi, se leggi istagram_nome scrivi istagram_nome
+        print("Password non trovata. Scrivi meglio.")
